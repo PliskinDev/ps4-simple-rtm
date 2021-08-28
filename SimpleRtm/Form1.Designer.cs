61ac5ff5ee8ba29fa2df687cee325819701c91be
mizdx <mizdx@outlook.com>
2021-08-28 04:09:21 +0300

    first commit

---
+++ SimpleRtm/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace SimpleRtm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(188, 111);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(34, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "192.168.1.2";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(202, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(138, 111);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cheats";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Connection Status:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Attach Status:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "N/A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(266, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "N/A";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 26);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Infinite Health";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(16, 51);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(89, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Infinite Ammo";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(16, 75);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(111, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Infinite Flash Light";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 149);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "2Dark Simple Rtm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SimpleRtm/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libdebug;
+namespace SimpleRtm
+{
+    public partial class Form1 : Form
+    {
+        private PS4DBG ps4;
+        private int pid;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.ps4 = new PS4DBG(this.textBox1.Text);
+                this.ps4.Connect();
+
+                foreach (libdebug.Process process in ps4.GetProcessList().processes)
+                {
+                    if (process.name == "eboot.bin")
+                    {
+                        pid = process.pid;
+                        ps4.Notify(222, "Connection!");
+                        this.label4.Text = "Connected";
+                        this.button1.ForeColor = Color.Green;
+                        this.label4.ForeColor = Color.Green;
+                        this.label5.Text = "Attached";
+                        this.label5.ForeColor = Color.Green;
+                        this.ps4.Notify(222, "Connected!");
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+                if (!this.ps4.IsConnected)
+                {
+                    MessageBox.Show("Failed To Connect and Attach!", "Connection Failed!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.label4.Text = "Failed";
+                    this.button1.ForeColor = Color.Red;
+                    this.label4.ForeColor = Color.Red;
+                    this.label5.Text = "Failed";
+                    this.label5.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.ps4 == null)
+                {
+                    MessageBox.Show("Not connected to PS4! Exiting", "Connection Error!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                    this.button2.ForeColor = Color.Red;
+                    this.label4.Text = "Not connected";
+                    this.label4.ForeColor = Color.Red;
+                    this.label5.Text = "Not connected";
+                    this.label5.ForeColor = Color.Red;
+                    return;
+                }
+            }
+            catch
+            {
+                this.button2.ForeColor = Color.Blue;
+                this.label4.Text = "Disconnected";
+                this.label4.ForeColor = Color.Blue;
+                this.label5.Text = "Disconnected";
+                this.label5.ForeColor = Color.Blue;
+                this.ps4.Notify(222, "Disconecting!");
+                this.ps4.Disconnect();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                byte[] HealthOn = new byte[] { 0xE9, 0x3C, 0x46, 0x2B, 0x00, 0x90, 0x90 };
+                this.ps4.WriteMemory(pid, 0x16F1BF, HealthOn);
+                ps4.Notify(222, "Infinite Health Enable [Credits Saso]");
+            }
+            else
+            {
+                byte[] HealthOn = new byte[] { 0x41, 0x89, 0x86, 0xC0, 0x00, 0x00, 0x00 };
+                this.ps4.WriteMemory(pid, 0x16F1BF, HealthOn);
+                ps4.Notify(222, "Infinite Health Disable [Credits Saso]");
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                byte[] AmmoOn = new byte[] { 0xE9, 0x75, 0x1A, 0x2B, 0x00, 0x90, 0x90, 0x90 };
+                this.ps4.WriteMemory(pid, 0x171D96, AmmoOn);
+                ps4.Notify(222, "Infinite Ammo Enable [Credits Saso]");
+            }
+            else
+            {
+                byte[] AmmoOff = new byte[] { 0x41, 0xFF, 0x8C, 0x24, 0x00, 0x04, 0x00, 0x00 };
+                this.ps4.WriteMemory(pid, 0x171D96, AmmoOff);
+                ps4.Notify(222, "Infinite Ammo Disable [Credits Saso]");
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                byte[] FlashLightOn = new byte[] { 0xE9, 0x52, 0x41, 0x2D, 0x00, 0x90, 0x90, 0x90 };
+                this.ps4.WriteMemory(pid, 0x14F6CB, FlashLightOn);
+                ps4.Notify(222, "Infinite Flash Light Enable [Credits Saso]");
+            }
+            else
+            {
+                byte[] FlashLightOff = new byte[] { 0x41, 0x89, 0x94, 0x24, 0x00, 0x04, 0x00, 0x00 };
+                this.ps4.WriteMemory(pid, 0x14F6CB, FlashLightOff);
+                ps4.Notify(222, "Infinite Flash Light Disable [Credits Saso]");
+            }
+        }
+    }
+}
+
